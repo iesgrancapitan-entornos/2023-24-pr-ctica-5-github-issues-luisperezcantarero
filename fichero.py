6466b2b1bc3bb37a3d2c3b4f2287df74bb87d7df
luisperezcantarero <a23pecalu@iesgrancapitan.org>
2024-04-16 21:16:24 +0200

    Cierre de issues (fix #3, fix #4)

--- fichero.py
+++ fichero.py
@@ -1,9 +1,14 @@
+"""
+Autor: Luis Pérez Cantarero
+
+Este programa suma 2 números y devuelve el resultado.
+"""
 def suma(a, b):
     resultado = a + b
     return resultado
 
-num1 = input("Ingrese el primer número: ")
-num2 = input("Ingrese el segundo número: ")
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
 
 resultado = suma(num1, num2)
 print("La suma es: ", resultado)
