59a20d8ac76251e92f48f5ccd94f07e21aae8bb8
luisperezcantarero <a23pecalu@iesgrancapitan.org>
2024-04-16 19:56:57 +0200

    Programa python totalmente bien hecho

---
+++ fichero.py
@@ -0,0 +1,8 @@
+def suma(a, b)
+    resultado = a + b
+
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+
+resultado = suma(num1, num2)
+print("La suma es: ", resultado)
